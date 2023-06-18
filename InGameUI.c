847264f2f080d3710dd4de30fdc6b8e436ade9cc
fellix1905 <daydreaming519@naver.com>
2023-06-18 22:33:56 +0900

    printStartScreen add

--- InGameUI.c
+++ InGameUI.c
@@ -5,6 +5,24 @@
 #include "UI.h"
 #include "Root.h"
 
-//int printStartScreen()
+int printStartScreen() {
+	DrawGameBoard(0);
+
+	Showcharacter(Character[0], GBOARD_ORIGIN_X + STARTING_POINT_WIDTH * 2, GBOARD_ORIGIN_Y + GBOARD_HEIGHT - STARTING_POINT_HEIGHT);
+
+	SetCurrentCursorPos(GBOARD_ORIGIN_X + 4, GBOARD_ORIGIN_Y + GBOARD_HEIGHT / 3);
+	printf("Press \'SPACE\' Start!");
+
+	int startButton;
+	while (1) {
+		startButton = _getch();
+		if (startButton == SPACE) {
+			break;
+		}
+	}
+	deleteScreen(2, GBOARD_HEIGHT / 3, 32, GBOARD_HEIGHT / 3);
+
+	return startButton;
+}
 //void clearScreen()
 //void deleteScreen(int x1, int y1, int x2, int y2)
